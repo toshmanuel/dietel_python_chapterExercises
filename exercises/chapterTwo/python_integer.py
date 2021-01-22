0f34fa0fe4a0b53c6c6bf5b8762f1288854e9c37
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-01-22 10:00:46 +0100

    integer values

---
+++ exercises/chapterTwo/python_integer.py
@@ -0,0 +1,10 @@
+int_number = int(input('enter base number'))
+int_exponent = int(input('enter exponent'))
+
+int_result = int_number ** int_exponent
+
+print(int_result)
+
+# (How Big Can Python Integers Be?) We’ll answer this question later in the book.
+# For now, use the exponentiation operator ** with large and very large exponents to produce some huge integers and assign those to the variable number to see if Python accepts
+# them. Did you find any integer value that Python won’t accept
