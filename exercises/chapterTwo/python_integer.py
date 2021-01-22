bdbe24ccb073a805eeb080790887d6beb7180118
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-01-22 10:03:51 +0100

    integer values

--- exercises/chapterTwo/python_integer.py
+++ exercises/chapterTwo/python_integer.py
@@ -4,7 +4,3 @@ int_exponent = int(input('enter exponent'))
 int_result = int_number ** int_exponent
 
 print(int_result)
-
-# (How Big Can Python Integers Be?) We’ll answer this question later in the book.
-# For now, use the exponentiation operator ** with large and very large exponents to produce some huge integers and assign those to the variable number to see if Python accepts
-# them. Did you find any integer value that Python won’t accept
